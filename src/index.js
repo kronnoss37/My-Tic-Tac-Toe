d9688cd2a2a2590dc16db67ee930534ab794b21a
kronnoss <kronnoss.it@gmail.com>
2026-01-16 19:49:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import initView from './appStates/view.js';
 import state from './appStates/model.js';
 import bindController from './appStates/controller.js';
 
-
 const elements = {
   mainContainerElement: document.querySelector('main > .container'),
+  notificationWrapperElement: document.querySelector('.notification-wrapper'),
 };
 const watchedState = initView(state, elements);
-bindController(watchedState, elements);
+bindController(watchedState);
