4084f2bdf2b51b4f68d5bbbf9be02dfcee470e53
kronnoss <kronnoss.it@gmail.com>
2026-01-16 19:54:18 +0300

    Update view.js, add a victory processing fun

--- src/appStates/view.js
+++ src/appStates/view.js
@@ -1,38 +1,45 @@
 import onChange from "../utils/onChange.js"
 import generateStartPage from '../pages/startPage.js'
-import {generateGamePage, generateGameFieldInner} from "../pages/gamePage.js"
-
-import players from "../config/playersConfig.js";
+import { generateGamePage, generateGameFieldInner, generateSidebar } from '../pages/gamePage.js';
+import notifyUser from "../services/notification.js";
 
 const switchPage = (watchedState, elements, currentPage = 'start') => {
   if (currentPage !== 'start' && currentPage !== 'game') return;
   currentPage === 'start' ? generateStartPage(watchedState, elements) : generateGamePage(watchedState, elements);
 };
 
-const renderGameStatusItem = ({ gameState: { currentPlayer} }, gameStatus) => {
-  if (gameStatus === 'start') return;
-  const gameMessage = gameStatus === 'draw' ? 'Ничья!' : `Победил: ${players[currentPlayer].name}`;
-  alert(gameMessage);
-};
+const showTheWinningCombination = (winningCombination, gameFieldElement) => {
+  if (!gameFieldElement) return;
+  if (winningCombination === null) return;
 
-const renderGameScoreItem = ({ gameState: { score } }, gameStatus) => {
-  const [firstPlayer, secondPlayer] = Object.keys(players)
-  console.log(firstPlayer);
-  console.log(secondPlayer);
-  const scoreMessage = `${players[firstPlayer].name} ${score[firstPlayer]} : ${score[secondPlayer]} ${players[secondPlayer].name}`;
-  alert(scoreMessage);
+  const classes = [
+    ['horizontal', 'horizontal--first'],
+    ['horizontal', 'horizontal--second'],
+    ['horizontal', 'horizontal--third'],
+    ['vertical', 'vertical--first'],
+    ['vertical', 'vertical--second'],
+    ['vertical', 'vertical--third'],
+    ['main-diagonal'],
+    ['secondary-diagonal'],
+  ];
+
+  const currentCombination = classes[winningCombination];
+  gameFieldElement.classList.add(...currentCombination);
+
+  requestAnimationFrame(() => {
+    gameFieldElement.classList.add(`${currentCombination[0]}__show`);
+  });
 };
 
 export default (state, elements) => {
-  const watchedState = onChange(state, (path, value, prev) => {
-    console.log('path', path);
+  const watchedState = onChange(state, (path, value) => {
     switch (path) {
       case 'pageStatus': {
         switchPage(watchedState, elements, value);
         break;
       }
       case 'gameState.gameStatus': {
-        renderGameStatusItem(watchedState, value);
+        notifyUser(elements, watchedState.gameState);
         break;
       }
       case 'gameState.gameField': {
@@ -40,13 +47,13 @@ export default (state, elements) => {
         generateGameFieldInner(watchedState, gameFieldElement);
         break;
       }
-      case 'gameState.score.X': {
-        renderGameScoreItem(watchedState);
+      case 'gameState.movesCount': {
+        generateSidebar(watchedState);
         break;
       }
-      case 'gameState.score.O': {
-        console.log('player2');
-        renderGameScoreItem(watchedState);
+      case 'gameState.winningCombination': {
+        const { gameFieldElement } = elements;
+        showTheWinningCombination(value, gameFieldElement);
         break;
       }
       default: {
